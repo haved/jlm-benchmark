93bd54991aa7d2201ac1b56dfbf522b74cce081a
Håvard Krogstie <krogstie.havard@gmail.com>
2026-03-23 10:03:16 +0100

    move dependencies to single file

--- apt-install-dependencies.sh
+++ apt-install-dependencies.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-sudo apt-get update
-sudo apt-get install \
+apt-get update
+apt-get install -y \
+    wget pipx python3-psutil python3-pandas python3-matplotlib python3-seaborn \
+    just bear make ninja-build g++ gfortran autoconf texinfo \
+    llvm-18-dev clang-18 clang-format-18 libgtest-dev \
+    lmod locales doxygen unzip \
+    \
+    \
     build-essential gcc-multilib gcc-mingw-w64 libasound2-dev libpulse-dev libdbus-1-dev \
     libfontconfig-dev libfreetype-dev libgnutls28-dev libgl-dev libunwind-dev \
     libx11-dev libxcomposite-dev libxcursor-dev libxfixes-dev libxi-dev libxrandr-dev \
@@ -9,4 +15,4 @@ sudo apt-get install \
     libxkbcommon-dev libxkbregistry-dev \
     libxaw7-dev xaw3dg-dev libgtk-3-dev libglib2.0-dev libtree-sitter-dev \
     libgif-dev libxpm-dev libjpeg-dev libtiff-dev libgnutls28-dev \
-    libmpfr-dev libxxhash-dev gawk flex bison unzip bear just
+    libmpfr-dev libxxhash-dev gawk flex bison
